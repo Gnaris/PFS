c2a31aa92b73026ee9acfb376a68252ad1d471f8
Gnaris <chen.boris@hotmail.fr>
2026-02-09 00:12:51 +0100

    f

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 import ParisFashionShop from "./entities/PFS.js"
 
-const pfs = await new ParisFashionShop().connect("princesse.fcenter@gmail.com", "0951869879Chen")
+const pfs = await new ParisFashionShop().connect("MAIL", "MDP")
 await pfs.refreshPage({page : 1, nbProduct : 1})
