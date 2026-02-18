1d39b72ba5eb13523137a56f438ca097d574eb0c
Gnaris <chen.boris@hotmail.fr>
2026-02-18 18:13:03 +0100

    gfh

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,10 @@ import ParisFashionShop from "./entities/ParisFashionShop.js"
 import ParisFashionShopOrder from "./entities/ParisFashionShopOrder.js"
 import ParisFashionShopProduct from "./entities/ParisFashionShopProduct.js"
 
-const session = await new ParisFashionShop().connect("princesse.fcenter@gmail.com", "0951869879Chen")
+const session = await new ParisFashionShop().connect("", "")
 
 const PFSProduct = new ParisFashionShopProduct(session.getToken())
-await PFSProduct.refreshPage({page : 1, nbProduct : 1, reference : "H31", blacklistRef : ["Y41VS1", "A144VS2", "J190", "D356", "A1944VS2", "ZC22", "ZC22A", "ZC22E", "BL280", "BL281"]})
+await PFSProduct.refreshPage({page : 1, nbProduct : 1})
 
 // const PFSOrder = new ParisFashionShopOrder(session.getToken())
 // await PFSOrder.getOrderSummary()
